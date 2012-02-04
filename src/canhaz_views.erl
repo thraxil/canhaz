86a909f5bd72643fd19961c23ad0c1d536d96d55
Anders Pearson <anders@columbia.edu>
2012-02-03 23:06:58 -0500

    send back full tree of results for arbitrary xpath

--- src/canhaz_views.erl
+++ src/canhaz_views.erl
@@ -8,39 +8,19 @@ urls() -> [
 
 remove_duplicates(L) ->
     sets:to_list(sets:from_list(L)).
-% extract content-length from the http headers
-content_length(Headers) ->
-    list_to_integer(proplists:get_value("content-length",Headers,"0")).
-
-%% abs url inside the same server ej: /img/image.png    
-full_url({Root,_Context},ComponentUrl=[$/|_]) ->
-    Root ++ ComponentUrl;
-
-%% full url ej: http://other.com/img.png
-full_url({_Root,_Context},ComponentUrl="http://"++_) ->
-    ComponentUrl;
-
-% everything else is considerer a relative path.. obviously its wrong (../img) 
-full_url({Root,Context},ComponentUrl) ->
-    Root ++ Context ++ "/" ++ ComponentUrl.
-
-% returns the  domain, and current context path. 
-% url_context("http://www.some.domain.com/content/index.html)
-%      -> {"http://www.some.domain.com", "/content"}
-url_context(URL) ->
-    {http,_,Root,_Port,Path,_Query} = http_uri:parse(URL), 
-    Ctx = string:sub_string(Path,1, string:rstr(Path,"/")),
-    {"http://"++Root,Ctx}.
 
 page_fetch(URL) ->
     case httpc:request(URL) of
         {ok,{_,_Headers,Body}} ->
-%%            got_page_info(URL,content_length(Headers),Body);
 	    {ok,Body};
         {error,Reason} -> 
             {error,Reason}
     end.
 
+results_to_proplist({Tag,Attrs,Children}) ->
+    {Tag,[{attrs,Attrs},{children,[results_to_proplist(R) || R <- Children]}]};
+results_to_proplist(Text) ->
+    {text,Text}.
 
 fetch('GET',Req) ->
     Url = canhaz_shortcuts:get_request_element('GET',Req,"url",""),
@@ -48,8 +28,7 @@ fetch('GET',Req) ->
     {ok,Body} = page_fetch(Url),
     Tree = mochiweb_html:parse(Body),
     Results = remove_duplicates(mochiweb_xpath:execute(XPath,Tree)),
-%    [Head|_Body] = element(3,Tree),
-%    [TitleElement|_OtherHead] = element(3,Head),
-%    [Title] = element(3,TitleElement),
-%    [First|Rest] = Imgs,
-    Req:ok({"application/json", mochijson2:encode([{"results",Results}])}).
+    Req:ok({"application/json", 
+	    mochijson2:encode([{"results",
+				[results_to_proplist(R) || R <- Results]
+			       }])}).
